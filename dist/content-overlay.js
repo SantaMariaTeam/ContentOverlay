be228ed894790da7f508b5a0eac4d5a37f7a0b9c
Stéphane Klein <contact@stephane-klein.info>
2015-11-01 00:20:52 +0100

    Append show and hide event support + use wrapper div to improve draggable support

--- dist/content-overlay.js
+++ dist/content-overlay.js
@@ -9,14 +9,28 @@ $.fn.contentOverlay = function(options) {
   var $selector = $(this.selector);
   var $overlay = $(options.overlay);
 
-  $('html').on('click', function(e) {
-    $selector.removeClass('content-overlay-outlined');
-    $overlay.hide();
-  });
-  $selector.on('click', function(e) {
-      e.stopPropagation();
+  function disable_overlay() {
+    var content_overlayed = $('.content-overlay-wrapper > *:first-child').first();
+    if (content_overlayed) {
       $selector.removeClass('content-overlay-outlined');
-      $overlay.css($(this).offset()).show();
-      $(this).addClass('content-overlay-outlined');
+      $(content_overlayed).unwrap();
+      $overlay.hide();
+      $overlay.appendTo('body');
+      $(content_overlayed).trigger(e = $.Event('hide', content_overlayed[0]));
+    }
+  }
+
+  $('html').on('click', disable_overlay);
+
+  $selector.on('click', function(e) {
+    e.stopPropagation();
+    disable_overlay();
+    var $this = $(this);
+    $this.wrap('<div class="content-overlay-wrapper"></div>');
+    $overlay.insertAfter(this);
+    $overlay.show();
+    $this.addClass('content-overlay-outlined');
+    $this.trigger(e = $.Event('show', this));
   });
+  return this;
 };
