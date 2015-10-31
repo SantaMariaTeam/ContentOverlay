f012583a96484384ac456b0c2d739e447b9c2ae4
Stéphane Klein <contact@stephane-klein.info>
2015-10-31 22:25:00 +0100

    First import

---
+++ dist/content-overlay.js
@@ -0,0 +1,22 @@
+$.fn.contentOverlay = function(options) {
+  options = $.extend(
+    {
+      overlay: '#content-overlay'
+    },
+    options
+  );
+
+  var $selector = $(this.selector);
+  var $overlay = $(options.overlay);
+
+  $('html').on('click', function(e) {
+    $selector.removeClass('content-overlay-outlined');
+    $overlay.hide();
+  });
+  $selector.on('click', function(e) {
+      e.stopPropagation();
+      $selector.removeClass('content-overlay-outlined');
+      $overlay.css($(this).offset()).show();
+      $(this).addClass('content-overlay-outlined');
+  });
+};
