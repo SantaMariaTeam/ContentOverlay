18d099c7702daab48f4352c08ed761e4a97dc194
Stéphane Klein <contact@stephane-klein.info>
2015-11-01 22:48:33 +0100

    Append « hide » method

--- dist/content-overlay.js
+++ dist/content-overlay.js
@@ -1,4 +1,10 @@
 $.fn.contentOverlay = function(options) {
+  var $selector = this;
+  if (options == 'hide') {
+    disable_overlay();
+    return
+  }
+
   options = $.extend(
     {
       overlay: '#content-overlay'
@@ -6,17 +12,21 @@ $.fn.contentOverlay = function(options) {
     options
   );
 
-  var $selector = this;
   var $overlay = $(options.overlay);
 
   function disable_overlay() {
-    var content_overlayed = $('.content-overlay-wrapper > *:first-child').first();
-    if (content_overlayed) {
-      $selector.removeClass('content-overlay-outlined');
-      $(content_overlayed).unwrap();
-      $overlay.hide();
-      $overlay.appendTo('body');
-      $(content_overlayed).trigger(e = $.Event('hide', content_overlayed[0]));
+    var $content_overlayed = $('.content-overlay-wrapper > *:first-child').first();
+    if ($content_overlayed.length > 0) {
+      $overlay = $content_overlayed.data('overlay');
+      $content_overlayed
+        .removeClass('content-overlay-outlined')
+        .unwrap();
+
+      $overlay
+        .hide()
+        .appendTo('body');
+
+      $content_overlayed.trigger(e = $.Event('hide', $content_overlayed[0]));
     }
   }
 
@@ -27,10 +37,15 @@ $.fn.contentOverlay = function(options) {
     disable_overlay();
     var $this = $(this);
     $this.wrap('<div class="content-overlay-wrapper"></div>');
-    $overlay.insertAfter(this);
-    $overlay.show();
-    $this.addClass('content-overlay-outlined');
-    $this.trigger(e = $.Event('show', this));
+    
+    $overlay
+      .insertAfter(this)
+      .show();
+
+    $this
+      .data('overlay', $overlay)
+      .addClass('content-overlay-outlined')
+      .trigger(e = $.Event('show', this));
   });
   return this;
 };
